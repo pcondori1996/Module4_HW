2725f73e9c91804ce84e5a3432a4a7e4314cc565
Peru PC <paulocondori1@gmail.com>
2022-06-30 00:34:17 -0400

    This is my third draft. I am almost done but I still have minor bugs before I turn it in. I also still need to review more of the topic. I got it to finish but I still dont understand why. Its cool though.. I am a 3 week old programmer.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 //alert('Enjoy the game and please grade nicely :)');
-
+//localStorage.clear();
 //Getting elements from HTMl page
 var secsRemaining = document.querySelector('#secsLeft');
 var correctScore = document.querySelector('#correctScore');
@@ -9,10 +9,13 @@ var instructions = document.querySelector('.instructions');
 var questionHolder = document.querySelector('.questionHolder');
 var correctScore = document.querySelector('#correctScore');
 var postQuiz = document.querySelector('.postQuiz');
+var headerElements = document.querySelector('.headerElements');
+var headerPostElements = document.querySelector('.headerPostElements');
 
 
 // GET THE LENGHT OF ANSWERS ARRAY
-console.log(Object.keys(quesAndAnswerLists));
+//console.log(Object.keys(quesAndAnswerLists));
+
 
 //Button Selectors
 var questionH4 = document.querySelector('#question');
@@ -23,7 +26,7 @@ var option4 = document.querySelector('.option4');
 
 //gobla carables
 var index = 0;
-var secCount = 30;
+var secCount = 1;
 var scoreCount = 0;
 
 //Applying CSS to properties
@@ -37,20 +40,26 @@ secsRemaining.setAttribute('style', 'background-color: black');
 
 //Obeject that holds array of the question and answer options for every button
 var quesAndAnswerLists = {
-    questions: ['Eu sinto sua falta!', 'Todavia no se como escribir JavaScript!', 
-    'Ik mis je', 'Comment vas-tu?', 'I still dont understand JavaScript!'],
+    questions: ['Eu sinto sua falta!', 'Todavia no se como escribir JavaScript!',
+        'Ik mis je', 'Comment vas-tu?', 'I still dont understand JavaScript!'],
     button1Ops: ['Portuguese', 'English', 'Italian', 'French', 'English'],
     button2Ops: ['English', 'French', 'Dutch', 'German', 'French'],
     button3Ops: ['Spanish', 'Spanish', 'Spanish', 'Italian', 'Portuguese'],
     button4Ops: ['French', 'Italian', 'English', 'Dutch', 'Spanish'],
     answers: ['Portuguese', 'Spanish', 'Dutch', 'French', 'English']
 }
-//First 
+
+// Vars made after objects were made.
+var lengthOfArray = quesAndAnswerLists.answers.length;
+
+
+//Event listener for the Start Button
 startButton.addEventListener("click", function () {
     questionHolder.classList.remove('hideElement');
     queAndOpts.classList.remove('hideElement');
     instructions.classList.add('hideElement');
 
+    //Display all questions and options at index 0
     displayQuesAndOpts();
 
 
@@ -61,14 +70,22 @@ startButton.addEventListener("click", function () {
 
         if (secCount <= 0) {
             clearInterval(quizTimerInterval);
+            secCount = 0;
+            secsRemaining.textContent = secCount;
             questionHolder.classList.add('hideElement');
             queAndOpts.classList.add('hideElement');
             postQuiz.classList.remove('hideElement');
+            headerElements.classList.add('hideElement');
+            headerPostElements.classList.remove('hideElement');
+            //secsRemaining.classList.add('hideElement');
+            //correctScore.classList.add('hideElement');
         }
     }, 1000);
 
 });
 
+
+//Event Listener put in place on div holding all the buttons
 queAndOpts.addEventListener('click', function (event) {
     if (event.target.matches('.buttons')) {
         if (quesAndAnswerLists.answers[index] === event.target.textContent) {
@@ -85,11 +102,88 @@ queAndOpts.addEventListener('click', function (event) {
     displayQuesAndOpts();
 })
 
+//Function to display question and options
 var displayQuesAndOpts = function () {
-   // if (index ==) {}
+
+    // IF condition set in place so when questions run out.
+    if (index == lengthOfArray) {
+        questionHolder.classList.add('hideElement');
+        queAndOpts.classList.add('hideElement');
+        postQuiz.classList.remove('hideElement');
+        headerElements.classList.add('hideElement');
+        headerPostElements.classList.remove('hideElement');
+        secCount = 0;
+        secsRemaining.textContent = secCount;
+    };
+
     questionH4.textContent = quesAndAnswerLists.questions[index];
     option1.textContent = quesAndAnswerLists.button1Ops[index];
     option2.textContent = quesAndAnswerLists.button2Ops[index];
     option3.textContent = quesAndAnswerLists.button3Ops[index];
     option4.textContent = quesAndAnswerLists.button4Ops[index];
 };
+
+
+// -------After Quiz Section (Local Storage of Score and Username--------------
+
+var typeName = document.querySelector('#textPlace');
+var enterButton = document.querySelector('#insertName');
+
+
+enterButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
+
+    var StoredDate = {
+        Usernames: typeName.value,
+        FinalScores: scoreCount
+    };
+
+    var getData1 = GetData(StoredDate);
+    getData1.sort(compareData);
+
+    if(getData1.length > 10) { 
+        getData1.pop();
+    }
+    ConnectData(getData1);
+    //setter
+    localStorage.setItem('StoredPastName', JSON.stringify(getData1));
+
+
+});
+
+var GetData = function (StoredDate) {
+    var StoredNames = JSON.parse(localStorage.getItem("StoredPastName"));
+
+    if (StoredNames === null) {
+        StoredNames = [StoredDate];
+    } else {
+        StoredNames.push(StoredDate);
+    }
+    return StoredNames
+}
+
+function compareData(a, b) {
+    //simple logic to sort the values in the highscores array by their score
+    const scoreA = a.FinalScores;
+    const scoreB = b.FinalScores;
+
+    var comparison = 0;
+    if (scoreA < scoreB) {
+        comparison = 1;
+    } else if (scoreA > scoreB) {
+        comparison = -1
+    }
+    return comparison;
+}
+
+var OLofData = document.querySelector(".OLofData");
+
+var ConnectData = function(getData1) {
+    for(i=0 ; i < getData1.length; i++) {
+        var listItem = document.createElement('il')
+        listItem.textContent = getData1[i].Usernames + " Score: " + getData1[i].FinalScores;
+        OLofData.appendChild(listItem);
+    }
+}
+
